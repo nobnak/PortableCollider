1d2c0f3a561307d4be9f7a0c9a89103fb66cb9aa
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-01-04 17:37:19 +0900

    no message

--- Assets/PortableCollider/Example/CollisionController.cs
+++ Assets/PortableCollider/Example/CollisionController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using nobnak.Geometry;
 
 public class CollisionController : MonoBehaviour {
 	public string tagCollider = "Collider";
